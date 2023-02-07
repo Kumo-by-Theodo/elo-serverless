3e1ff825f4134c2ae4ca67f371caa641943ed995
Clement Marcilhacy <clementmarcilhacy@gmail.com>
2023-02-07 23:42:16 +0100

    feat: improve step function

--- lib/StateMachineConstruct.ts
+++ lib/StateMachineConstruct.ts
@@ -1,5 +1,4 @@
 import { RemovalPolicy } from 'aws-cdk-lib';
-import { Key } from 'aws-cdk-lib/aws-kms';
 import { LogGroup } from 'aws-cdk-lib/aws-logs';
 import {
   IStateMachine,
@@ -10,30 +9,118 @@ import {
   Pass,
   Map,
   Succeed,
+  TaskInput,
 } from 'aws-cdk-lib/aws-stepfunctions';
+
 import { Construct } from 'constructs';
-import { Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import { Role } from 'aws-cdk-lib/aws-iam';
+import {
+  DynamoAttributeValue,
+  DynamoGetItem,
+  LambdaInvoke,
+} from 'aws-cdk-lib/aws-stepfunctions-tasks';
+import { Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Code, Runtime, Function } from 'aws-cdk-lib/aws-lambda';
 
 export class StateMachineConstruct extends Construct {
   public stateMachine: IStateMachine;
 
-  constructor(scope: Construct, id: string, pipeRole: Role) {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: { pipeRole: Role; table: Table },
+  ) {
     super(scope, id);
 
-    const map = new Map(this, 'Map State', {
+    const succeed = new Succeed(this, 'Succeed');
+
+    const parsePayload = new Pass(this, 'Parse Payload', {
+      parameters: {
+        payloadEvent: JsonPath.stringToJson(
+          JsonPath.stringAt('$.dynamodb.NewImage.Payload.S'),
+        ),
+      },
+    });
+
+    const transformToArray = new Pass(this, 'Transform To Array', {
+      parameters: {
+        playersArray: JsonPath.array(
+          JsonPath.stringAt('$.payloadEvent.PlayerA.S'),
+          JsonPath.stringAt('$.payloadEvent.PlayerB.S'),
+        ),
+      },
+    });
+
+    const subtraction = new Pass(this, 'Subtract Scores', {
+      parameters: {
+        diff: JsonPath.mathAdd(
+          JsonPath.numberAt('$.firstRanking'),
+          JsonPath.numberAt('$.secondRanking'),
+        ),
+      },
+    });
+
+    const transformPayload = new Pass(this, 'Transform Payload', {
+      parameters: {
+        firstRanking: JsonPath.stringToJson(
+          JsonPath.format('-{}', JsonPath.stringAt('$[0].Item.ELO.N')),
+        ),
+        secondRanking: JsonPath.stringToJson(
+          JsonPath.stringAt('$[1].Item.ELO.N'),
+        ),
+      },
+    });
+
+    const dynamodbGetItem = new DynamoGetItem(this, 'Dynamo Get Item', {
+      key: {
+        PK: DynamoAttributeValue.fromString(JsonPath.stringAt('$')),
+      },
+      table: props.table,
+    });
+
+    const computeELOScore = new Function(this, 'Compute ELO Score', {
+      handler: 'index.handler',
+      code: Code.fromInline(`
+              exports.handler = ({diff}, _, callback) => {
+                callback(null, 1 / (1+10**(diff/400)));
+              };
+            `),
+      runtime: Runtime.NODEJS_16_X,
+    });
+    const lambdaInvokeComputeELOScore = new LambdaInvoke(
+      this,
+      'Lambda Invoke Compute ELO Score',
+      {
+        lambdaFunction: computeELOScore,
+      },
+    );
+
+    // Inner map
+    const mapGetItems = new Map(this, 'Map Get Items', {
+      itemsPath: JsonPath.stringAt('$.playersArray'),
+    });
+
+    // Outer map
+    const mapStreamEvents = new Map(this, 'Map Stream Events', {
       itemsPath: JsonPath.stringAt('$'),
     });
 
-    map.iterator(
-      new Pass(this, 'PassState', {
-        inputPath: JsonPath.stringAt('$.dynamodb.NewImage.Payload.S'),
-      }),
-    );
-
-    map.next(new Succeed(this, 'Succeed'));
+    mapStreamEvents
+      .iterator(
+        parsePayload
+          .next(transformToArray)
+          .next(
+            mapGetItems
+              .iterator(dynamodbGetItem)
+              .next(transformPayload)
+              .next(subtraction)
+              .next(lambdaInvokeComputeELOScore),
+          ),
+      )
+      .next(succeed);
 
     this.stateMachine = new StateMachine(this, 'TargetExpressStateMachine', {
-      definition: map,
+      definition: mapStreamEvents,
       stateMachineType: StateMachineType.EXPRESS,
       logs: {
         destination: new LogGroup(this, 'TargetLogs', {
@@ -45,6 +132,6 @@ export class StateMachineConstruct extends Construct {
       },
     });
 
-    this.stateMachine.grantStartSyncExecution(pipeRole);
+    this.stateMachine.grantStartSyncExecution(props.pipeRole);
   }
 }
